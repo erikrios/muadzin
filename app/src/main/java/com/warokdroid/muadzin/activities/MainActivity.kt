632631a720d32dc267f5557fd0beed2220fdf1de
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2019-11-25 15:03:03 +0700

    Add Search View to Filter Recycler View

--- app/src/main/java/com/warokdroid/muadzin/activities/MainActivity.kt
+++ app/src/main/java/com/warokdroid/muadzin/activities/MainActivity.kt
@@ -1,13 +1,93 @@
 package com.warokdroid.muadzin.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.ProgressBar
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
 import com.warokdroid.muadzin.R
+import com.warokdroid.muadzin.adapters.CityAdapter
+import com.warokdroid.muadzin.apis.ApiRepository
+import com.warokdroid.muadzin.models.City
+import com.warokdroid.muadzin.presenters.CityMainPresenter
+import com.warokdroid.muadzin.views.CityMainView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CityMainView {
+
+    private lateinit var rvCities: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var presenter: CityMainPresenter
+    private val cities: MutableList<City> = mutableListOf()
+    private lateinit var cityAdapter: CityAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+        val gson = Gson()
+        val request = ApiRepository()
+        presenter = CityMainPresenter(this, request, gson)
+        presenter.getCityList()
+        setActionBar(getString(R.string.choose_city))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        val searchFilter: MenuItem? = menu?.findItem(R.id.item_search_filter)
+        val searchView: SearchView = searchFilter?.actionView as SearchView
+        searchView.imeOptions = EditorInfo.IME_ACTION_DONE
+        searchView.queryHint = resources.getString(R.string.city_search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean = false
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText?.isEmpty()!!)
+                    presenter.getCityList()
+                cityAdapter.filter.filter(newText)
+                return false
+            }
+        })
+        return true
+    }
+
+    private fun initViews() {
+        rvCities = findViewById(R.id.rv_cities)
+        progressBar = findViewById(R.id.progress_bar)
+    }
+
+    private fun setRecyclerList() {
+        cityAdapter = CityAdapter(this, cities)
+        rvCities.setHasFixedSize(true)
+        rvCities.layoutManager = LinearLayoutManager(this)
+        rvCities.adapter = cityAdapter
+    }
+
+    override fun showLoading() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    override fun hideLoading() {
+        progressBar.visibility = View.GONE
+    }
+
+    override fun showCityList(data: List<City>?) {
+        cities.clear()
+        data?.let { cities.addAll(it) }
+        setRecyclerList()
+        cityAdapter.notifyDataSetChanged()
+    }
+
+    private fun setActionBar(title: String?) {
+        if (supportActionBar != null)
+            (supportActionBar as ActionBar).title = title
     }
 }
--- app/src/main/java/com/warokdroid/muadzin/adapters/CityAdapter.kt
+++ app/src/main/java/com/warokdroid/muadzin/adapters/CityAdapter.kt
@@ -1,27 +1,85 @@
 package com.warokdroid.muadzin.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.warokdroid.muadzin.R
 import com.warokdroid.muadzin.models.City
 
-class CityAdapter(val context: Context, val cities: MutableList<City>) :
-    RecyclerView.Adapter<CityAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+class CityAdapter(
+    private val context: Context,
+    private val cities: MutableList<City>
+) :
+    RecyclerView.Adapter<CityAdapter.ViewHolder>(), Filterable {
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    private val citiesFull: List<City> = cities
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
+        LayoutInflater.from(context).inflate(
+            R.layout.city_item, parent, false
+        )
+    )
+
+    override fun getItemCount(): Int = cities.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bindItems(cities[position]) {
+            Toast.makeText(context, "${it.name} clicked!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val tvCityFirstLetter: TextView = view.findViewById(R.id.tv_city_first_letter)
+        private val tvName: TextView = view.findViewById(R.id.tv_city_name)
+
+        fun bindItems(
+            city: City,
+            listener: (City) -> Unit
+        ) {
+
+            tvCityFirstLetter.text = city.name?.first().toString()
+            tvName.text = city.name?.toLowerCase()?.capitalize()
+
+            itemView.setOnClickListener { listener(city) }
+        }
+    }
+
+    override fun getFilter(): Filter = searchFilter
+
+    private val searchFilter: Filter = object : Filter() {
+        override fun performFiltering(constraint: CharSequence?): FilterResults {
+            val filteredList: MutableList<City> = mutableListOf()
+
+            if (constraint == null || constraint.isEmpty())
+                filteredList.addAll(citiesFull)
+            else {
+                val filterPattern = constraint.toString().toLowerCase().trim()
+
+                for (item in citiesFull) {
+                    if (item.name?.let { it.toLowerCase().contains(filterPattern) }!!)
+                        filteredList.add(item)
+                }
+            }
+
+            val results = FilterResults()
+            results.values = filteredList
+
+            return results
+        }
+
+        override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+            cities.clear()
+            cities.addAll(results?.values as MutableList<City>)
+            notifyDataSetChanged()
+        }
 
     }
 }
