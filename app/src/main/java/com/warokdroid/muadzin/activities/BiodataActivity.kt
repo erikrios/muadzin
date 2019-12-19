e15671a7e83106136b49a6eeda0ca148559789a2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2019-12-19 07:37:57 +0700

    Add biodata activity

---
+++ app/src/main/java/com/warokdroid/muadzin/activities/BiodataActivity.kt
@@ -0,0 +1,13 @@
+package com.warokdroid.muadzin.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.warokdroid.muadzin.R
+
+class BiodataActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_biodata)
+    }
+}
